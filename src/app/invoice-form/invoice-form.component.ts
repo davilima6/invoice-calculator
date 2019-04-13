971ab0a05ef81b74d7824550021097d1799c0fe7
Davi Lima <davilima6@gmail.com>
2019-04-13 10:47:20 +0200

    Add two-way data model binding to invoice form

--- src/app/invoice-form/invoice-form.component.ts
+++ src/app/invoice-form/invoice-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Invoice } from '../data/invoice';
 
 @Component({
   selector: 'app-invoice-form',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InvoiceFormComponent implements OnInit {
 
+  invoice: Invoice = {
+    customer: null,
+    start: null,
+    end: null
+  }
+
   constructor() { }
 
   ngOnInit() {
   }
-
 }
