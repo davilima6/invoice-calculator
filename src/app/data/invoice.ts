0aafe4c1668060d8f1584377a3f05966da60845a
Davi Lima <davilima6@gmail.com>
2019-04-13 16:07:44 +0200

    Expand Invoice model to describe total price amount, list and number of orders

--- src/app/data/invoice.ts
+++ src/app/data/invoice.ts
@@ -1,5 +1,10 @@
+import { Order } from '../invoice/order';
+
 export interface Invoice {
     customer_id: string,
     start_date: string,
-    end_date: string
-};
+    end_date: string,
+    amount: number,
+    orders: Order[],
+    ordersNumber: number
+}
