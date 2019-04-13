1dad779cd89a1dddb08003279d0fefa8fa906de8
Davi Lima <davilima6@gmail.com>
2019-04-13 10:01:23 +0200

    Add form for invoice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InvoiceFormComponent } from './invoice-form/invoice-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InvoiceFormComponent
   ],
   imports: [
     BrowserModule,
