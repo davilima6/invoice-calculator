5d78934c433efdec9b7e3925b34e7dc06344d26f
Davi Lima <davilima6@gmail.com>
2019-04-15 10:01:18 +0200

    Add more typings

--- src/app/data/data.service.ts
+++ src/app/data/data.service.ts
@@ -2,7 +2,10 @@ import { Injectable, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { SESSION_STORAGE, StorageService } from 'ngx-webstorage-service';
+
+import { Customer } from './customer';
 import { Invoice } from './invoice';
+import { Order } from '../invoice/order';
 
 const API = {
   baseUrl: 'https://private-2a91cb-byrd1.apiary-mock.com',
@@ -25,29 +28,29 @@ export class DataService {
     @Inject(SESSION_STORAGE) private storage: StorageService
   ) { }
 
-  getCustomers(): Observable<any> {
-    return this.http.get(`${API.baseUrl}/${API.customers}`);
+  getCustomers(): Observable<Customer[]> {
+    return this.http.get(`${API.baseUrl}/${API.customers}`) as Observable<Customer[]>;
   }
 
-  getCustomerOrders(invoice: Invoice): Observable<any> {
+  getCustomerOrders(invoice: Invoice): Observable<Order[]> {
     return this.http.get(`${API.baseUrl}/${API.orders}/${API.customers}`, {
       params: {
         customer_id: invoice.customer_id,
         start_date: invoice.start_date,
         end_date: invoice.end_date
       }
-    });
+    }) as Observable<Order[]>;
   }
 
-  getCache(key: string, defaultValue: any = undefined) {
+  getCache(key: string, defaultValue: any = undefined): any | undefined {
     return this.storage.has(key) ? this.storage.get(key) : defaultValue;
   }
 
-  setCache(key: string, value: any) {
+  setCache(key: string, value: any): void {
     this.storage.set(key, value);
   }
 
-  clearCache() {
+  clearCache(): boolean {
     this.storage.clear();
 
     return true;
